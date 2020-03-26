00149dc9237249cc3b4fa92242e00ed2873c69bb
mssalkhalifah <mohammad.alkhalifah121@gmail.com>
2020-03-26 19:07:33 +0300

    Finished writing and testing the InsertionSort class.

--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -1,2 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Random;
+
 public class InsertionSortTest {
+    @Test
+    public void test() {
+        int[] arr = new int[100];
+        Random random = new Random();
+
+        for (int i = 0; i < 1000000; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = random.nextInt(101);
+            }
+            arr = InsertionSort.insertionSort(arr);
+            Assert.assertEquals(isSortedInc(arr), true);
+        }
+    }
+
+    private static boolean isSortedInc(int[] arr) {
+        int number = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (number > arr[i])
+                return false;
+            number = arr[i];
+        }
+        return true;
+    }
 }
