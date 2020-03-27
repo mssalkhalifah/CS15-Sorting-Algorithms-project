c2435372d356ef8ab95df4f4469d9e9e4829ea1b
mssalkhalifah <mohammad.alkhalifah121@gmail.com>
2020-03-27 18:33:14 +0300

    Wrote a constructor for the Heap class and a method that returns the root.

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,2 +1,23 @@
 public class Heap {
+    private final int ROOT;
+    private int size;
+    private int[] arr;
+
+    /**
+     * Initializes the array for the heap.
+     * @param size The total size of the heap.
+     */
+    public Heap(int size) {
+        this.ROOT = 0;
+        this.size = size;
+        this.arr = new int[size];
+    }
+
+    /**
+     * Returns the root of the heap tree.
+     * @return The root of the tree.
+     */
+    public int getMin() {
+        return arr[ROOT];
+    }
 }
