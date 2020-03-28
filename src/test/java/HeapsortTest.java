6bdbda33c66fe63cf5f47c4ddc52cf35cf6cf09d
mssalkhalifah <mohammad.alkhalifah121@gmail.com>
2020-03-28 12:26:22 +0300

    Heapsort has been tested

--- src/test/java/HeapsortTest.java
+++ src/test/java/HeapsortTest.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
 public class HeapsortTest {
+    @Test
+    public void test() {
+        int[] arr = new int[500];
+        Random random = new Random();
+
+        for (int i = 0; i < 1000000; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = random.nextInt(101);
+            }
+            Heapsort.heapsort(arr);
+            Assert.assertTrue(InsertionSortTest.isSortedInc(arr));
+        }
+    }
 }
--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -8,16 +8,16 @@ public class InsertionSortTest {
         int[] arr = new int[100];
         Random random = new Random();
 
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 10; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[j] = random.nextInt(101);
             }
-            arr = InsertionSort.insertionSort(arr);
-            Assert.assertEquals(isSortedInc(arr), true);
+            InsertionSort.insertionSort(arr);
+            Assert.assertTrue(isSortedInc(arr));
         }
     }
 
-    private static boolean isSortedInc(int[] arr) {
+    public static boolean isSortedInc(int[] arr) {
         int number = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (number > arr[i])
